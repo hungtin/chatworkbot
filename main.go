05cb7e7b18fb71d4c1a2d1f725bce38b1f344a18
Tin Trinh <trinh.tin@hamee.co.jp>
2018-10-13 23:33:50 +0900

    Add /chatwork endpoint

--- main.go
+++ main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
 )
+
+func chatworkHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer r.Body.Close()
+
+	fmt.Println(body)
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -15,6 +27,8 @@ func main() {
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello Chatwork Bot"))
 	})
+
+	router.HandleFunc("/chatwork", chatworkHandlerFunc)
 
 	fmt.Println("Server start to listen on ", port)
 	http.ListenAndServe(":"+port, router)
