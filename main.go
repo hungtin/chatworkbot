cfdc9d18a347a3094f3f2715cfd86203e3190be8
Tin Trinh <trinh.tin@hamee.co.jp>
2018-10-13 22:47:34 +0900

    Listen on port from ENV

--- main.go
+++ main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello Chatwork Bot"))
 	})
 
-	fmt.Println("Server start to listen")
-	http.ListenAndServe(":80", router)
+	fmt.Println("Server start to listen on ", port)
+	http.ListenAndServe(":"+port, router)
 }
